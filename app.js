5c754e85706348a8e1d2822216e0054907f9535f
sourabh <sourabhpande43@gmail.comm>
2023-06-26 16:41:00 +0530

    Inject - some middleware & swaggerUi

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 require("dotenv").config();
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
 
-// Ref: ✈️🔗https://expressjs.com/en/5x/api.html#router
+//SWAGGER DOCUMENTATION
+const swaggerUi = require("swagger-ui-express");
+const fs = require("fs");
+const YAML = require("yamljs");
+const file = fs.readFileSync("./swagger.yaml", "utf8");
+const swaggerDocument = YAML.parse(file);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// BRINGS ROUTES 
+//MORGAN MIDDLEWARE
+const morgan = require("morgan");
+app.use(morgan("tiny"));
+
+//COOKIES AND FILE-UPLOAD MIDDLEWARE
+app.use(cookieParser());
+app.use(fileUpload());
+
+//REGULAR MIDDLEWARE
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+// BRINGS ROUTES
 const home = require("./routes/home");
 
 // USING MIDDLEWARE
-app.use("/api/v1",home);
+app.use("/api/v1", home);
 
 module.exports = app;
---
+++ map_project.js
@@ -0,0 +1,29 @@
+/*
+@ABOUT_ROUTES:
+Ref: ✈️🔗https://expressjs.com/en/5x/api.html#router
+
+-----------------NEW--------------------
+@SECTION:Injecting Docs and MIDDLEWARE
+@ABOUT: All about Inject MIDDLEWARE
+@TITLE:
+@LOCATION:🗃️App.js
+
+@OVERVIEW:->
+@REQUERE
+-🎗️Need Body-parser for handle JSON
+-🎗️Need cookie-parser,swagger,morgan
+
+@ABOUT_MORGAN
+Ref: ✈️🔗 https://www.npmjs.com/package/morgan
+SCROLL ON till Bottom ...above link
+😗@NOTE: NEEDs to be come befoure route it's convection
+
+@INVITE_&_INSTALL:-> MORGAN,cookieParser,fileUpload,swaggerUi & some REGULAR MIDDLEWARE like json & urlencoded 
+
+
+
+ 
+
+
+
+*/
